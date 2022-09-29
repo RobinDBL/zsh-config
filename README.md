1dbca1785619aed542a0dd4d0975f58732cba3ce
Robin Deblauwe <r.deblauwe@televic.com>
2022-09-29 22:31:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ZSH RobinDBL
+
+This is my custom ZSH configuration. This config inclueds autosuggestions, autocorrection and a custom theme (one of those fancy ones).
+
+## Install
+
+Replace all files under the `custom` folder in `~/.oh-my-zsh/custom` with the files in this repo.
---
+++ customrc.zsh
@@ -0,0 +1,27 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+# Define aliasses
+alias projects="~/Documents/projects/"
+alias quantum="~/Documents/projects/quantum/"
+alias test="npm run test"
+alias serve="ng serve --host 0.0.0.0"
+alias Downloads="~/Downloads"
+alias Documents="~/Documents"
+alias webserver="python3 -m http.server"
+alias thcsb-login="az acr login -n thcsb"
+alias keycloak="docker start keycloak"
+
+
+# Enable spell checker
+setopt correct
+# add a promt if incorrect
+export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color? [y/n] "
