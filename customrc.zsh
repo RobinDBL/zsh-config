d832079451edd0e2d4be3be5c815c1dcd8c80f99
Robin Deblauwe <r.deblauwe@televic.be>
2025-01-24 11:12:50 +0100

    Feat: Added custom .zshrc

--- customrc.zsh
+++ customrc.zsh
@@ -10,8 +10,8 @@
 #
 
 # Define aliasses
-alias projects="~/Documents/projects/"
-alias quantum="~/Documents/projects/quantum/"
+alias projects="/mnt/data/projects/"
+alias quantum="/mnt/data/projects/quantum/"
 alias test="npm run test"
 alias serve="ng serve --host 0.0.0.0"
 alias Downloads="~/Downloads"
@@ -21,6 +21,14 @@ alias thcsb-login="az acr login -n thcsb"
 alias keycloak="docker start keycloak"
 alias du="dust"
 alias apt="apt-fast"
+alias vim="nvim"
+alias devops="/mnt/data/projects/devops"
+alias experiments="/mnt/data/projects/experiments"
+alias fusion="/mnt/data/projects/fusion"
+alias production="/mnt/data/projects/production"
+alias shared="/mnt/data/projects/shared"
+alias templates="/mnt/data/projects/templates"
+alias tools="/mnt/data/projects/tools"
 
 # Custom env
 
@@ -32,3 +40,4 @@ setopt correct
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color? [y/n] " 
 
 export PYTHONPATH="${PYTHONPATH}:/home/robin/.local/bin"
+export NODE_ENV="development"
